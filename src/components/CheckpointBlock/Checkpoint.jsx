f27f245d031a2592f563ae1396d3222e42404def
Tempesto20 <Bro74rus1@mail.ru>
2022-10-19 14:27:31 +0300

    пофиксил баг в fullCat

--- src/components/CheckpointBlock/Checkpoint.jsx
+++ src/components/CheckpointBlock/Checkpoint.jsx
@@ -4,7 +4,7 @@ import { setCheckpoinValue } from '../../redux/slices/filterSlice';
 import styles from './checkpoint.module.scss';
 
 const checkpointList = [
-  { name: 'Все', value: 'Купить' && "Продан" },
+  { name: 'Все', value: 'Купить' && 'Продан' },
   { name: 'В наличии', value: 'Купить' },
   { name: 'Отсутсвуют в продаже', value: 'Продан' },
 ];
@@ -13,16 +13,14 @@ function Checkpoint() {
   const dispatch = useDispatch();
 
   const checkpointValue = useSelector((state) => state.filterSlice.checkpoint);
-console.log(checkpointValue)
-const arr = useSelector((state) => state.catsSlice.items);
-console.log(arr)
-
+  console.log(checkpointValue);
+  const arr = useSelector((state) => state.catsSlice.items);
+  console.log(arr);
 
   const checkboxHandler = (event) => {
     console.log(event.target.value);
     dispatch(setCheckpoinValue(event.target.value));
   };
-
 
   return (
     <div className={styles.background}>
@@ -30,29 +28,20 @@ console.log(arr)
         <div className={styles.container}>
           <p>Группировать по:</p>
 
-          <div className={styles.eventList}
-           onChange={checkboxHandler}
-           >
+          <div className={styles.eventList} onChange={checkboxHandler}>
             {checkpointList.map((item, index) => {
               return (
-                <div className={styles.eventTitle} key={index} name={item.name}>
-                  <input value={item.value} type="radio"  checked
-                  // onClick={(items) => checkboxHandler(items)}
-                  />
-                  <label className={styles.subtitle} for={item.name}>{item.name}</label>
-                </div>
+                <div>
+                <input type="radio" 
+                name={item.name} 
+                value={item.value} 
+                checked 
+                />
+                <label for={item.name} key={index}>{item.name}</label>
+              </div>
               );
             })}
-              <div>
-      <input type="radio" id="huey" name="drone" value="huey"
-             checked />
-      <label for="huey">Huey</label>
-    </div>
-    <div>
-      <input type="radio" id="huey" name="drone" value="huey"
-             checked />
-      <label for="huey">Huey</label>
-    </div>
+          
           </div>
         </div>
       </div>
@@ -61,6 +50,34 @@ console.log(arr)
 }
 
 export default Checkpoint;
+
+{/* <div>
+<input type="radio" id="huey" name="drone" value="huey" checked />
+<label for="huey">Huey</label>
+</div>
+<div>
+<input type="radio" id="huey" name="drone" value="huey" checked />
+<label for="huey">Huey</label>
+</div> */}
+
+
+{/* <div className={styles.eventTitle} key={index} name={item.name}>
+                  <input
+                    value={item.value}
+                    type="radio"
+                    checked
+                    // onClick={(items) => checkboxHandler(items)}
+                  />
+                  <label className={styles.subtitle} for={item.name}>
+                    {item.name}
+                  </label>
+                </div> */}
+
+
+
+
+
+
 
 {
   /* <div className={styles.eventList} onChange={checkboxHandler}>
