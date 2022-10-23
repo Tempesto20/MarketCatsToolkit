c98b3ca29e6417e15bdeb6d60f29a738d546b11d
Tempesto20 <Bro74rus1@mail.ru>
2022-10-23 19:58:36 +0300

    внёс коррективы в scss - homa, допилил скелетон для хома

--- src/components/CatsBlock/Skeleton.tsx
+++ src/components/CatsBlock/Skeleton.tsx
@@ -1,30 +1,33 @@
 import React from "react"
 import ContentLoader from "react-content-loader"
 
+import styles from './skeleton.module.scss';
+
 const Skeleton = () => (
 
-<ContentLoader 
-speed={2}
-width={280}
-height={500}
-viewBox="0 0 380 500"
-backgroundColor="#f3f3f3"
-foregroundColor="#ecebeb"
+
+  <ContentLoader 
+  speed={2}
+  width={380}
+  height={500}
+  viewBox="0 0 380 500"
+  backgroundColor="#f3f1f1"
+  foregroundColor="#ecebeb"
+  className={styles.skeleton}
 >
-<rect x="0" y="0" rx="0" ry="0" width="280" height="220" /> 
-<rect x="0" y="240" rx="0" ry="0" width="280" height="80" /> 
-<rect x="0" y="350" rx="0" ry="0" width="280" height="52" /> 
-<rect x="0" y="430" rx="32" ry="32" width="280" height="60" />
+  <rect x="0" y="11" rx="0" ry="0" width="380" height="253" /> 
+  <rect x="-1" y="273" rx="0" ry="0" width="380" height="130" /> 
+  <rect x="0" y="413" rx="30" ry="30" width="383" height="60" />
 </ContentLoader>
+
+
 
 
 )
 
 export default Skeleton;
 
-
-/*
-<ContentLoader 
+{/* <ContentLoader 
 speed={2}
 width={280}
 height={500}
@@ -36,36 +39,20 @@ foregroundColor="#ecebeb"
 <rect x="0" y="240" rx="0" ry="0" width="280" height="80" /> 
 <rect x="0" y="350" rx="0" ry="0" width="280" height="52" /> 
 <rect x="0" y="430" rx="32" ry="32" width="280" height="60" />
-</ContentLoader>
-*/
-
-/*
-  <ContentLoader 
-    speed={2}
-    width={260}
-    height={500}
-    viewBox="0 0 280 500"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-  >
-    <rect x="0" y="0" rx="0" ry="0" width="260" height="422" /> 
-    <rect x="0" y="433" rx="32" ry="32" width="260" height="60" />
-  </ContentLoader>
-  */
+</ContentLoader> */}
 
 
-    // <ContentLoader 
-  //   className="pizza-block"
-  //   speed={2}
-  //   width={380}
-  //   height={400}
-  //   viewBox="0 0 280 500"
-  //   backgroundColor="#f3f3f3"
-  //   foregroundColor="#ecebeb"
-  // >
-  //   <circle cx="134" cy="136" r="125" /> 
-  //   <rect x="0" y="279" rx="10" ry="10" width="280" height="25" /> 
-  //   <rect x="0" y="326" rx="10" ry="10" width="280" height="90" /> 
-  //   <rect x="0" y="436" rx="10" ry="10" width="95" height="30" /> 
-  //   <rect x="125" y="427" rx="24" ry="24" width="152" height="45" />
-  // </ContentLoader>
+
+
+{/* <ContentLoader 
+speed={2}
+width={280}
+height={500}
+viewBox="0 0 280 500"
+backgroundColor="#f3f3f3"
+foregroundColor="#ecebeb"
+className="windh"
+>
+<rect x="0" y="0" rx="0" ry="0" width="280" height="422" /> 
+<rect x="0" y="423" rx="32" ry="32" width="280" height="60" />
+</ContentLoader> */}
