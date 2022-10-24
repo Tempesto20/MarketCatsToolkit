74a0927b1fc4a4510cd051d4b13f4149ccd2c7d2
Tempesto20 <Bro74rus1@mail.ru>
2022-10-24 15:37:46 +0300

    доработал fullCat со скелетоном

--- src/Page/FullCat/Skeleton.tsx
+++ src/Page/FullCat/Skeleton.tsx
@@ -4,36 +4,19 @@ import ContentLoader from 'react-content-loader';
 import styles from './skeleton.module.scss';
 
 const Skeleton = () => (
-  <ContentLoader
+  <ContentLoader 
     speed={2}
-    width={1200}
-    height={520}
-    viewBox="0 0 1200 520"
+    width={1280}
+    height={500}
+    viewBox="0 0 1280 500"
     backgroundColor="#f3f1f1"
     foregroundColor="#ecebeb"
-    // className={styles.skeleton}
-    >
-    <rect x="0" y="-1" rx="0" ry="0" width="1200" height="480" />
-
-    <circle cx="285" cy="506" r="4" />
-    <circle cx="325" cy="506" r="4" />
-    <circle cx="365" cy="506" r="4" />
-    <circle cx="405" cy="506" r="4" />
-    <circle cx="445" cy="506" r="4" />
-    <circle cx="485" cy="506" r="4" />
-    <circle cx="525" cy="506" r="4" />
-    <circle cx="565" cy="506" r="4" />
-
-    <circle cx="605" cy="506" r="4" />
-    
-    <circle cx="645" cy="506" r="4" />
-    <circle cx="685" cy="506" r="4" />
-    <circle cx="725" cy="506" r="4" />
-    <circle cx="765" cy="506" r="4" />
-    <circle cx="805" cy="506" r="4" />
-    <circle cx="845" cy="506" r="4" />
-    <circle cx="885" cy="506" r="4" />
-    <circle cx="925" cy="506" r="4" />
+  >
+    <rect x="640" y="100" rx="0" ry="0" width="180" height="40" /> 
+    <rect x="640" y="160" rx="0" ry="0" width="240" height="40" /> 
+    <rect x="640" y="240" rx="0" ry="0" width="600" height="160" /> 
+    <rect x="640" y="460" rx="0" ry="0" width="215" height="32" /> 
+    <rect x="20" y="100" rx="0" ry="0" width="600" height="400" />
   </ContentLoader>
 );
 
